d0acecb09aec4a74155d57eb2e4e86a3278607d3
khkim <khsyee@gmail.com>
2019-02-26 01:45:40 +0900

    Modify dqn mario code to practice code

--- dqn_mario/network.py
+++ dqn_mario/network.py
@@ -12,45 +12,13 @@ class DQN:
         self.net_name = net_name
 
         with tf.variable_scope(self.net_name + 'input_layer'):
-            self.input_ = tf.placeholder(tf.float32, shape=(
-                None, self.state_size[0], self.state_size[1], self.state_size[2])
-            )
+            self.input_ =  # placeholder 구현
         self.q_value = self._build_network()
 
     def _build_network(self):
         with tf.variable_scope(self.net_name):
-            conv1 = layer.conv2d(
-                inputs=self.input_,
-                num_outputs=32,
-                kernel_size=[8, 8],
-                stride=[4, 4],
-            )
-            conv2 = layer.conv2d(
-                inputs=conv1,
-                num_outputs=64,
-                kernel_size=[4, 4],
-                stride=[2, 2],
-            )
-            conv3 = layer.conv2d(
-                inputs=conv2,
-                num_outputs=64,
-                kernel_size=[3, 3],
-            )
-            conv3_flatten = layer.flatten(conv3)
+            """
+            network 구현 
+            """
 
-            fc1 = layer.fully_connected(
-                inputs=conv3_flatten,
-                num_outputs=128,
-                activation_fn=tf.nn.relu,
-            )
-            fc2 = layer.fully_connected(
-                inputs=fc1,
-                num_outputs=128,
-                activation_fn=tf.nn.relu,
-            )
-            q_value = layer.fully_connected(
-                inputs=fc2,
-                num_outputs=self.action_size,
-                activation_fn=None,
-            )
             return q_value
