d27ea96db0eea61891e8b7542eff8c44097df60b
khkim <khsyee@gmail.com>
2019-02-27 01:31:17 +0900

    Merge master
    Squashed commit of the following:
    
    commit fe24fa93a2985d9430fadc255f33deb834d9ccb7
    Author: khkim <khsyee@gmail.com>
    Date:   Wed Feb 27 01:20:04 2019 +0900
    
        Add verify code in per dqn
    
    commit af2d73f60ea75dad5c852e8e26cd785e97664467
    Author: khkim <khsyee@gmail.com>
    Date:   Wed Feb 27 01:01:45 2019 +0900
    
        Add Adv DQN on CartPole
    
    commit aaa682ea85e4cb440558c550b65ede0f8064cab3
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 10:28:13 2019 +0900
    
        Add img for classic rl
    
    commit 7f5f7ffcc80baed1ce9952291617d03f3c96437b
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 09:33:55 2019 +0900
    
        Fix dqn_pong
    
    commit f0f31ea581d0f6c7d3e8c9bc357f9a66d982e5b8
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 09:27:26 2019 +0900
    
        Modify dqn_pong episode
    
    commit e7859cec73236b0412d7fae1f2568e5172f25ef0
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 09:12:05 2019 +0900
    
        Modify run.py for loading model
    
    commit f58e4a674e6f2ee9675194bb4935e2e1cd715e83
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 02:15:05 2019 +0900
    
        Add classic rl
    
    commit d0acecb09aec4a74155d57eb2e4e86a3278607d3
    Author: khkim <khsyee@gmail.com>
    Date:   Tue Feb 26 01:45:40 2019 +0900
    
        Modify dqn mario code to practice code

--- dqn_mario/agent.py
+++ dqn_mario/agent.py
@@ -25,81 +25,34 @@ class DQNAgent:
         self.n_epsilon_decay = 100000
         self.epsilon = self.s_epsilon
 
+        # replay buffer
+        self.buffer = ReplayBuffer(50000)
+
         # place holder
         self.actions = tf.placeholder(tf.int32, shape=None)
         self.targets = tf.placeholder(tf.float32, shape=None)
 
         # network
-        self.policy_net = DQN(self.state_size, self.action_size, net_name="policy_net")
-        self.target_net = DQN(self.state_size, self.action_size, net_name="target_net")
+        self.policy_net = DQN({})
+        self.target_net = DQN({})
         self.sess.run(tf.global_variables_initializer())
         self.update_target_network()
-
-        # replay buffer
-        self.buffer = ReplayBuffer(50000)
 
         # optimizer
         self.loss_op, self.train_op = self._build_op()
 
     def _build_op(self):
         """신경망 학습을 위한 Loss function과 Optimaizer를 정의합니다."""
-        action_one_hot = tf.one_hot(self.actions, self.action_size, 1.0, 0.0)
-        predict_q = tf.reduce_sum(tf.multiply(self.policy_net.q_value, action_one_hot), axis=1)
-
-        loss_op = tf.reduce_mean(tf.square(self.targets - predict_q))
-        train_op = tf.train.RMSPropOptimizer(
-            learning_rate=self.learning_rate,
-            decay=0.95,
-            momentum=0.95,
-            epsilon=0.01
-        ).minimize(loss_op)
-        return loss_op, train_op
 
     def select_action(self, state):
         """epsilon-greedy로 action을 선택합니다."""
-        # epsilon greedy policy
-        if self.epsilon > np.random.random():
-            selected_action = np.random.randint(self.action_size)
-        else:
-            state = np.expand_dims(state, axis=0)
-            selected_action = self.sess.run(
-                self.policy_net.q_value,
-                feed_dict={self.policy_net.input_: state}
-            )
-            selected_action = np.argmax(selected_action, axis=1)
-            selected_action = selected_action[0]
-
-        # 매 step마다 epsilon을 줄여나갑니다.
-        if self.epsilon >= self.e_epsilon:
-            self.epsilon -= (self.s_epsilon - self.e_epsilon) / self.n_epsilon_decay
-        return selected_action
 
     def update_model(self):
         """학습 네트워크를 학습합니다."""
-        states, actions, rewards, next_states, dones = self.buffer.sample(self.batch_size)
-
-        target_q = self.target_net.q_value.eval({self.target_net.input_: next_states}, self.sess)
-        target_q = np.max(target_q, axis=1)
-        targets = rewards + self.discount_factor * target_q * (1. - dones)
-
-        loss, _ = self.sess.run(
-            [self.loss_op, self.train_op],
-            feed_dict={self.policy_net.input_: states,
-                       self.actions: actions,
-                       self.targets: targets})
-
-        return loss
 
     def update_target_network(self):
         """학습 네트웍의 변수의 값들을 타겟 네트웍으로 복사해서 타겟 네트웍의 값들을 최신으로 업데이트합니다."""
-        copy_op = []
 
-        main_q_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="policy_net")
-        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="target_net")
-
-        for main_q_var, target_var in zip(main_q_vars, target_vars):
-            copy_op.append(target_var.assign(main_q_var.value()))
-        self.sess.run(copy_op)
 
     def save_model(self, filename):
         """Save model."""
